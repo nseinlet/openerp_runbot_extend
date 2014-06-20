279b6c1a171f4c66d79a88a16a2ef8785991c6fb
Nicolas Seinlet <nse@openerp.com>
2014-06-20 16:22:54 +0200

    manage error levels

--- runbot_restore_db/runbot.py
+++ runbot_restore_db/runbot.py
@@ -1,23 +1,43 @@
 from openerp.osv import fields, osv
+from openerp.runbot import runbot
+
+loglevels = (('none', 'None'),
+             ('warning', 'Warning'),
+             ('error', 'Error'))
+
+#_re_error = r'^(?:\d{4}-\d\d-\d\d \d\d:\d\d:\d\d,\d{3} \d+ (?:ERROR|CRITICAL) )|(?:Traceback \(most recent call last\):)$'
+#_re_error = r'^.*FAILED.*$'
+#_re_warning = r'^\d{4}-\d\d-\d\d \d\d:\d\d:\d\d,\d{3} \d+ WARNING '
 
 class runbot_repo(osv.Model):
     _inherit = "runbot.repo"
 
     _columns = {
+        'fallback_id': fields.many2one('runbot.repo', 'Fallback repo', domain=['|',('active','=', True),('active','=', False)]),
         'db_name': fields.char("Database name to replicate"),
-        'no_build': fields.boolean('Do not buid'),
+        'active': fields.boolean('Do not buid'),
         'sequence': fields.integer('Sequence of display', select=True),
+        'error': fields.selection(loglevels, 'Error messages'),
+        'critical': fields.selection(loglevels, 'Critical messages'),
+        'traceback': fields.selection(loglevels, 'Traceback messages'),
+        'warning': fields.selection(loglevels, 'Warning messages'),
+        'failed': fields.selection(loglevels, 'Failed messages'),
     }
 
     _defaults = {
-        'no_build': False,
+        'active': True,
+        'error': 'error',
+        'critical': 'error',
+        'traceback': 'error',
+        'warning': 'warning',
+        'failed': 'none',
     }
 
     _order = 'sequence'
 
     def update_git(self, cr, uid, repo, context=None):
         super(runbot_repo, self).update_git(cr, uid, repo, context)
-        if repo.no_build:
+        if not repo.active:
             bds = self.pool['runbot.build']
             bds_ids = bds.search(cr, uid, [('repo_id', '=', repo.id), ('state', '=', 'pending')], context=context)
             bds.write(cr, uid, bds_ids, {'state': 'done'}, context=context)
@@ -38,3 +58,44 @@ class runbot_build(osv.osv):
         cmd, mods = build.cmd()
         cmd += ['-d', '%s-all' % build.dest, '-u', 'all', '--stop-after-init', '--log-level=debug']
         return self.spawn(cmd, lock_path, log_path, cpu_limit=None)
+
+    def job_30_run(self, cr, uid, build, lock_path, log_path):
+        runbot._re_error = self._get_regexeforlog(build=build, errlevel='error')
+        runbot._re_warning = self._get_regexeforlog(build=build, errlevel='warning')
+
+        super(runbot_build, self).job_30_run(cr, uid, build, lock_path, log_path)
+
+    def _get_regexeforlog(self, build, errlevel):
+        addederror = False
+        regex = r'\d{4}-\d\d-\d\d \d\d:\d\d:\d\d,\d{3} \d '
+        if build.repo_id.error == errlevel:
+            if addederror:
+                regex += "|"
+            else:
+                addederror = True
+            regex +="(ERROR)"
+        if build.repo_id.critical == errlevel:
+            if addederror:
+                regex += "|"
+            else:
+                addederror = True
+            regex +="(CRITICAL)"
+        if build.repo_id.warning == errlevel:
+            if addederror:
+                regex += "|"
+            else:
+                addederror = True
+            regex +="(WARNING)"
+        if build.repo_id.failed == errlevel:
+            if addederror:
+                regex += "|"
+            else:
+                addederror = True
+            regex +="(TEST.*FAIL)"
+        if build.repo_id.traceback == errlevel:
+            if addederror:
+                regex = '(Traceback \(most recent call last\))|(%s)' % regex
+            else:
+                regex = '(Traceback \(most recent call last\))'
+        #regex = '^' + regex + '$'
+        return regex
